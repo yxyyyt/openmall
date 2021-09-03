c2eaa00aea7bf69c3f642290058d43383feb4c4f
yxyyyt <yxyyyt@126.com>
2021-09-03 11:37:49 +0800

    feat: Add redis cache for carousel

--- openmall-api/src/main/java/com/sciatta/openmall/api/controller/IndexController.java
+++ openmall-api/src/main/java/com/sciatta/openmall/api/controller/IndexController.java
@@ -7,41 +7,66 @@ import com.sciatta.openmall.api.pojo.vo.CategoryItemVO;
 import com.sciatta.openmall.api.pojo.vo.CategoryVO;
 import com.sciatta.openmall.api.pojo.vo.SubCategoryVO;
 import com.sciatta.openmall.common.JSONResult;
+import com.sciatta.openmall.common.constants.RedisCacheConstants;
 import com.sciatta.openmall.common.enums.YesOrNo;
+import com.sciatta.openmall.common.utils.JsonUtils;
 import com.sciatta.openmall.service.CarouselService;
 import com.sciatta.openmall.service.CategoryService;
 import com.sciatta.openmall.service.pojo.dto.CarouselDTO;
 import com.sciatta.openmall.service.pojo.dto.CategoryDTO;
 import com.sciatta.openmall.service.pojo.dto.CategoryItemDTO;
 import com.sciatta.openmall.service.pojo.dto.SubCategoryDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by yangxiaoyu on 2021/7/31<br>
  * All Rights Reserved(C) 2017 - 2021 SCIATTA<br><p/>
  * 首页
  */
+@Slf4j
 @RestController
 @RequestMapping("index")
 public class IndexController {
     private final CarouselService carouselService;
     private final CategoryService categoryService;
+    private final StringRedisTemplate redisTemplate;
     
-    public IndexController(CarouselService carouselService, CategoryService categoryService) {
+    public IndexController(CarouselService carouselService, CategoryService categoryService, StringRedisTemplate redisTemplate) {
         this.carouselService = carouselService;
         this.categoryService = categoryService;
+        this.redisTemplate = redisTemplate;
     }
     
     @GetMapping("carousels")
     public JSONResult carousels() {
+        List<CarouselVO> carouselsVO;
+        
+        // 从缓存中获取
+        String carouselFromCache = redisTemplate.opsForValue().get(RedisCacheConstants.CAROUSEL);
+        if (StringUtils.hasText(carouselFromCache)) {
+            log.debug("cache {} has text: {}", RedisCacheConstants.CAROUSEL, carouselFromCache);
+            carouselsVO = JsonUtils.jsonToList(carouselFromCache, CarouselVO.class);
+            return JSONResult.success(carouselsVO);
+        }
+        
         List<CarouselDTO> carouselDTOList = carouselService.queryAll(YesOrNo.YES.type);
         
-        List<CarouselVO> carouselsVO = CarouselConverter.INSTANCE.carouselDTOListToCarouselVOList(carouselDTOList);
+        carouselsVO = CarouselConverter.INSTANCE.carouselDTOListToCarouselVOList(carouselDTOList);
+        
+        // 设置缓存
+        String value = JsonUtils.objectToJson(carouselsVO);
+        redisTemplate.opsForValue().set(RedisCacheConstants.CAROUSEL, value);
+        log.debug("set cache {}: {}", RedisCacheConstants.CAROUSEL, value);
+        
         return JSONResult.success(carouselsVO);
     }
     
---
+++ openmall-common/src/main/java/com/sciatta/openmall/common/constants/RedisCacheConstants.java
@@ -0,0 +1,10 @@
+package com.sciatta.openmall.common.constants;
+
+/**
+ * Created by yangxiaoyu on 2021/9/3<br>
+ * All Rights Reserved(C) 2017 - 2021 SCIATTA<br><p/>
+ * RedisCacheConstants
+ */
+public class RedisCacheConstants {
+    public static final String CAROUSEL = "carousel";
+}
